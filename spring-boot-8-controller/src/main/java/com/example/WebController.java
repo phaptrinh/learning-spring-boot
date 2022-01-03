eb0c9652befa76823591ed528f06c97ad5ee2403
phaptrinh <phap.n.trinh00@gmail.com>
2022-01-03 11:08:51 +0700

    add lesson 8

---
+++ spring-boot-8-controller/src/main/java/com/example/WebController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/hello")
+    public String hello(@RequestParam(name="name", required = false, defaultValue = "") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
